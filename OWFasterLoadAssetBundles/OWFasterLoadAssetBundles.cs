6b3b445c1cf74ad45119a0dfb824afca19a19988
JohnCorby <williamforgames6@gmail.com>
2024-06-17 22:10:30 -0700

    set up modbehavior

--- OWFasterLoadAssetBundles/OWFasterLoadAssetBundles.cs
+++ OWFasterLoadAssetBundles/OWFasterLoadAssetBundles.cs
@@ -12,27 +12,6 @@ public class OWFasterLoadAssetBundles : ModBehaviour
 	public void Awake()
 	{
 		Instance = this;
-		// You won't be able to access OWML's mod helper in Awake.
-		// So you probably don't want to do anything here.
-		// Use Start() instead.
-	}
-
-	public void Start()
-	{
-		// Starting here, you'll have access to OWML's mod helper.
-		ModHelper.Console.WriteLine($"My mod {nameof(OWFasterLoadAssetBundles)} is loaded!", MessageType.Success);
-
-		new Harmony("JohnCorby.OWFasterLoadAssetBundles").PatchAll(Assembly.GetExecutingAssembly());
-
-		// Example of accessing game code.
-		OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen); // We start on title screen
-		LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
-	}
-
-	public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
-	{
-		if (newScene != OWScene.SolarSystem) return;
-		ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+		Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
 	}
 }
-
